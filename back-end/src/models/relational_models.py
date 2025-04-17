f011b4edd8622f555ab7b1f4bcdbe89fa032eaf1
Amirreza <realamirrezajoulani@gmail.com>
2025-04-17 15:22:48 +0430

    update relational_models.py

--- back-end/src/models/relational_models.py
+++ back-end/src/models/relational_models.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 from uuid import uuid4, UUID
 
-from sqlmodel import Column, DateTime, Field, Relationship, func
+from sqlmodel import Column, DateTime, Field, Relationship, SQLModel, func
+from schemas.base.job_seeker_education import JobSeekerEducationBase
 from schemas.base.job_seeker_personal_information import JobSeekerPersonalInformationBase
 from schemas.base.job_seeker_resume import JobSeekerResumeBase
+from schemas.base.job_seeker_skill import JobSeekerSkillBase
+from schemas.base.job_seeker_work_experience import JobSeekerWorkExperienceBase
 from schemas.base.user import UserBase
 
 
-class User(UserBase, table=True):
+class DefaultFields(SQLModel):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
-
-    password: str = Field(...)
 
     created_at: datetime = Field(
         sa_column=Column(DateTime(timezone=True), server_default=func.now()),
@@ -19,6 +20,13 @@ class User(UserBase, table=True):
     updated_at: datetime | None = Field(
         sa_column=Column(DateTime(timezone=True), onupdate=func.now()),
     )
+
+    class Config:
+        table = False
+
+
+class User(DefaultFields, UserBase, table=True):
+    password: str = Field(...)
 
     job_seeker_resumes: list["JobSeekerResume"] = Relationship(
         back_populates="user",
@@ -27,41 +35,60 @@ class User(UserBase, table=True):
     )
 
 
-class JobSeekerPersonalInformation(JobSeekerPersonalInformationBase, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
-
+class JobSeekerPersonalInformation(DefaultFields, JobSeekerPersonalInformationBase, table=True):
     job_seeker_resumes: list["JobSeekerResume"] = Relationship(
         back_populates="job_seeker_personal_information",
         cascade_delete=True,
         sa_relationship_kwargs={"lazy": "selectin"}
     )
 
-    created_at: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=func.now()),
-    )
 
-    updated_at: datetime | None = Field(
-        sa_column=Column(DateTime(timezone=True), onupdate=func.now()),
-    )
-
-
-class JobSeekerResume(JobSeekerResumeBase, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
-
+class JobSeekerResume(DefaultFields, JobSeekerResumeBase, table=True):
     user: User = Relationship(
         back_populates="job_seeker_resumes",
         sa_relationship_kwargs={"lazy": "selectin"}
     )
 
-    created_at: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=func.now()),
-    )
-
-    updated_at: datetime | None = Field(
-        sa_column=Column(DateTime(timezone=True), onupdate=func.now()),
-    )
-
     job_seeker_personal_information: JobSeekerPersonalInformation = Relationship(
         back_populates="job_seeker_resumes",
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+    job_seeker_skills: list["JobSeekerSkill"] = Relationship(
+        back_populates="resume",
+        cascade_delete=True,
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+    job_seeker_work_experiences: list["JobSeekerWorkExperience"] = Relationship(
+        back_populates="resume",
+        cascade_delete=True,
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+    job_seeker_educations: list["JobSeekerEducation"] = Relationship(
+        back_populates="resume",
+        cascade_delete=True,
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+
+class JobSeekerSkill(DefaultFields, JobSeekerSkillBase, table=True):
+    resume: JobSeekerResume = Relationship(
+        back_populates="job_seeker_skills",
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+
+class JobSeekerWorkExperience(DefaultFields, JobSeekerWorkExperienceBase, table=True):
+    resume: JobSeekerResume = Relationship(
+        back_populates="job_seeker_work_experiences",
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+
+class JobSeekerEducation(DefaultFields, JobSeekerEducationBase, table=True):
+    resume: JobSeekerResume = Relationship(
+        back_populates="job_seeker_educations",
         sa_relationship_kwargs={"lazy": "selectin"}
     )

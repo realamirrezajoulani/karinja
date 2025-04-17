c17667b489399ce8e9a7e04fe30e0ea7ed9cb57f
Amirreza <realamirrezajoulani@gmail.com>
2025-04-17 19:09:17 +0430

    feat: create employer_company.py, image.py and job_posting.py

---
+++ back-end/src/schemas/employer_company.py
@@ -0,0 +1,54 @@
+from uuid import UUID
+from datetime import datetime
+
+from sqlmodel import BIGINT, Column, Field, SQLModel
+
+from schemas.base.employer_company import CompanyBase
+from utilities.enumerables import EmployerCompanyEmployeeCount, EmployerCompanyIndustry, EmployerCompanyOwnershipType, JobSeekerEducationDegree
+
+
+class CompanyPublic(CompanyBase):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime | None
+
+
+class CompanyCreate(CompanyBase):
+    pass
+
+
+class CompanyUpdate(SQLModel):
+    # ge=10000000, le=999999999999
+    registration_number: int = Field(
+        unique=True,
+        sa_column=Column(BIGINT)
+    )
+
+    # min_length=5, max_length=30
+    full_name: str = Field(...)
+
+    # min_length=5, max_length=64
+    summary: str | None = Field(default=None)
+
+    industry: EmployerCompanyIndustry = Field(...)
+
+    ownership_type: EmployerCompanyOwnershipType = Field(...)
+
+    # pattern = https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
+    website_address: str | None = Field(default=None)
+
+    # ge=(Present year) - 200, le=(Present year)
+    founded_year: int = Field(...)
+
+    employee_count: EmployerCompanyEmployeeCount = Field(...)
+
+    # min_length=5, max_length=255
+    address: str = Field(...)
+
+    phone: int = Field(
+        unique=True,
+        sa_column=Column(BIGINT)
+    )
+
+    # min_length=30, max_length=2048
+    description: str = Field(...)
---
+++ back-end/src/schemas/image.py
@@ -0,0 +1,24 @@
+from uuid import UUID
+from datetime import datetime
+
+from sqlmodel import Field, SQLModel
+
+from schemas.base.image import ImageBase
+from utilities.enumerables import ImageType
+
+class ImagePublic(ImageBase):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime | None
+
+
+class ImageCreate(ImageBase):
+    pass
+
+
+class ImageUpdate(SQLModel):
+    title: ImageType = Field(...)
+
+    # min_length=5, max_length=30
+    url: str = Field(...)
+

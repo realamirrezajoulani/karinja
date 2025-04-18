31955fa286eb1b09969fc5f15bc5528722d84924
Amirreza <realamirrezajoulani@gmail.com>
2025-04-18 12:05:51 +0430

    create 1 file and update 1 file

---
+++ back-end/src/schemas/base/job_application.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+from sqlmodel import Field, SQLModel
+
+from utilities.enumerables import JobApplicationStatus
+
+
+class JobApplicationBase(SQLModel):
+    # Present date
+    application_date: str = Field(...)
+
+    # From one day to 6 months
+    status: JobApplicationStatus = Field(...)
+
+    # min_length=5, max_length=250
+    cover_letter: str | None = Field(default=None)
+
+    job_posting_id: UUID = Field(foreign_key="jobposting.id", ondelete="CASCADE")
+
+    job_seeker_resume_id: UUID = Field(foreign_key="jobseekerresume.id", ondelete="CASCADE")

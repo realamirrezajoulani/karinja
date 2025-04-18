e54046de61c2e20121dd3a86204846eb5ecfae74
Amirreza <realamirrezajoulani@gmail.com>
2025-04-18 12:06:10 +0430

    update job_posting.py

---
+++ back-end/src/schemas/job_posting.py
@@ -0,0 +1,47 @@
+from uuid import UUID
+from datetime import datetime
+
+from sqlmodel import BIGINT, Column, Field, SQLModel
+
+from schemas.base.job_posting import JobPostingBase
+from utilities.enumerables import IranProvinces, JobPostingEmploymentType, JobPostingJobCategory, JobPostingSalaryUnit, JobPostingStatus, JobSeekerEducationDegree
+
+
+class JobPostingPublic(JobPostingBase):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime | None
+
+
+class JobPostingCreate(JobPostingBase):
+    pass
+
+
+class JobPostingUpdate(SQLModel):
+    # min_length=5, max_length=30
+    title: str = Field(...)
+
+    location: IranProvinces = Field(...)
+
+    # min_length=20, max_length=2048
+    job_description: str = Field(...)
+
+    employment_type: JobPostingEmploymentType = Field(...)
+
+    # Present date
+    posted_date: str = Field(...)
+
+    # From one day to 6 months
+    expiry_date: str | None = Field(default=None)
+
+    salary_unit: JobPostingSalaryUnit = Field(...)
+
+    # If the null value is entered, it means that the salary is negotiable
+    salary_range: int | None = Field(default=None, sa_column=Column(BIGINT))
+
+    job_categoriy: JobPostingJobCategory = Field(...)
+
+    # ge=1, le=100
+    vacancy_count: int = Field(...)
+
+    status: JobPostingStatus = Field(...)

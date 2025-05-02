05a0f93539a23caca055870545f3056e216b2a7c
Amirreza <realamirrezajoulani@gmail.com>
2025-05-02 20:23:22 +0430

    update config.py

--- back-end/src/config.py
+++ back-end/src/config.py
@@ -34,7 +34,8 @@ origins = [
     "http://localhost:3000",
     "https://localhost:3000",
     "http://localhost:5173",
-    "https://localhost:5173"
+    "https://localhost:5173",
+    "https://karinja-gold.vercel.app/"
 ]
 
 app.add_middleware(

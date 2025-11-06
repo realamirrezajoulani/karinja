e2153230eb64a10fd34d688576a5f1e30fbc7fd6
Amirreza Joulani <realamirrezajoulani@gmail.com>
2025-11-07 05:39:40 +1100

    update config.py

--- back-end/src/config.py
+++ back-end/src/config.py
@@ -35,6 +35,8 @@ origins = [
     "https://localhost:3000",
     "http://localhost:5000",
     "https://localhost:5000"
+    "http://localhost:5173",
+    "https://localhost:5173"
 ]
 
 app.add_middleware(
